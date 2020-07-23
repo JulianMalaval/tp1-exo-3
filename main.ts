647436d36461b4ca63393babcf2acd9eee10f6fd
JulianMalaval <68005754+JulianMalaval@users.noreply.github.com>
2020-07-23 20:49:20 +1100

    Mise à jour main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,18 +1,18 @@
 let x = 0
-let y = 0
+let y = randint(0, 4)
 basic.forever(function () {
-    basic.pause(1000)
-    led.stopAnimation()
+    basic.pause(100)
     led.unplot(x, y)
-    led.plotBrightness(x, y, 150)
     y += 1
     led.plot(x, y)
     if (y > 4) {
         y = -1
         x = randint(0, 4)
     }
-    if (led.point(x, 2)) {
-        led.plotBrightness(x, y - 1, 170)
-        led.plotBrightness(x, y - 2, 80)
+    if (led.point(x, y)) {
+        led.plot(x, y + 1)
+        led.plot(x, y + 2)
+        led.plotBrightness(x, y, 80)
+        led.plotBrightness(x, y + 1, 170)
     }
 })
